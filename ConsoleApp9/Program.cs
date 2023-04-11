6b9f990346d84fdacfcf4aadcbc4ab141d93585f
nargespoorhatamii <126357653+nargespoorhatamii@users.noreply.github.com>
2023-04-11 22:45:10 +0430

    Add project files.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace project
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("inter an integer number(x)");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("inter an integer number(y)");
+            int y = Convert.ToInt32(Console.ReadLine());
+            calc(x, y);
+
+        }//end main
+        private static void calc(int x, int y)
+        {
+            Double Result = Math.Sqrt(Math.Abs(x) + Math.Pow(y, 2));
+            Console.WriteLine(Result);
+        }//end function
+
+    }//end class
+}//end name space
